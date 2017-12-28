df98e6986b4fb586d27fb59391d07ffec65b5e98
duanyunfeng@yy.com <duanyunfeng@yy.com>
2017-12-28 16:01:16 +0800

    init more

--- httpserver/main.go
+++ httpserver/main.go
@@ -5,20 +5,15 @@ import (
 	"github.com/astaxie/beego/orm"
 	"log"
 	_ "github.com/yy-java/cnt2/httpserver/routers"
-	"time"
 )
 
 func main() {
-	
 
 	log.Println("cnt2 runmode", beego.BConfig.RunMode)
 	log.Println("etcd::endpoints", beego.AppConfig.Strings("etcd.endpoints"))
 	log.Println("mysql::dataSource", beego.AppConfig.Strings("mysql.dataSource"))
 
 	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("mysql.dataSource"), 30, 100)
-
 	beego.Run()
 
-
-	time.Sleep(100 * time.Second)
 }
